2f9ac46e858ed44aa480a689039429aa89974601
AnaSantos7 <asantos@csumb.edu>
2020-07-14 23:35:40 -0700

    retrieved data from weather api

---
+++ weather-app/src/App.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Titles from './components/Titles';
+import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = 'dbb3fb3b59ebc9c7edbd7f3749b1d8e6';
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined, 
+    country: undefined, 
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description,
+      erro: ''
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Titles></Titles>
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+      </div>
+    );
+  }
+};
+
+export default App;
---
+++ weather-app/src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Weather extends React.Component {
+    render() {
+        return(
+            <div>
+                {this.props.city}
+                {this.props.country}
+                {this.props.temperature}
+                {this.props.humidity}
+                {this.props.description}
+                
+            </div>
+        );
+    }
+};
+
+export default Weather;
