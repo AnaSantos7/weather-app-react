7ddb77fcb9fdc3966a4426c2cf4ac714c35b554e
AnaSantos7 <asantos@csumb.edu>
2020-07-15 00:40:26 -0700

    display api info

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -21,15 +21,37 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      erro: ''
-    });
+    
+    if (city && country) {
+      if (data.cod == 404) {
+        this.setState({
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          error: "Input does not match any known location!"
+        });   
+      } else {
+        this.setState({
+          temperature: data.main.temp,
+          city: data.name,
+          country: data.sys.country,
+          humidity: data.main.humidity,
+          description: data.weather[0].description,
+          error: ""
+        });
+      }
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: 'Please enter the value.'
+      });
+    }
   }
   render() {
     return (
--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -1,14 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Weather Finder</h1>
-                
-            </div>
-        );
-    }
-};
+const Titles = () => (
+    <div>
+        <h1>Weather Finder</h1>
+        
+    </div>
+);
 
 export default Titles;
